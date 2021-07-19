355253d8cb5a42a5c010e7300cec1f3abe2bf28d
alberto <albertozanola1996@gmail.com>
2021-07-19 13:26:08 +0200

    Scaffolding

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//Scrivi un programma che stampi i numeri da 1 a 100
+
+
+
+
+/*
+-Per i multipli di 3 stampi “Fizz”;
+-Per i multipli di 5 stampi Buzz;
+-Per i  multipli di 3 e 5 stampi FizzBuzz;
+*/
